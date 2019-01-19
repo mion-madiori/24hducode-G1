3b91a39b620d81be9f69d3606467ca7f989b4719
mdupdup <malo.dupont@ynov.com>
2019-01-19 21:40:22 +0100

    working json, debugging on_message

--- poc5_getCapteursInfo.py
+++ poc5_getCapteursInfo.py
@@ -1,27 +1,44 @@
 import paho.mqtt.client as mqtt
+import time
+import json
 
 
-def on_connect(client, user_data, flags, rc):
-    print("Successfully connected with result code" + str(rc))
+def on_connect(client, userdata, flags, rc):
+    print("Successfully connected with result code " + str(rc))
 
-    client.publish("laumio/status/advertise")
+    client.subscribe("laumio/status/advertise")
 
-    client.subscribe("laumio/all/discover")
-    client.publish("/laumio/all/animate_rainbow")
+    print("Liste des laumios :")
+    client.publish("laumio/all/discover")
+    client.subscribe("atmosphere/temperature")
+
+    client.publish("laumio/Laumio_0FC168/json", payload="{'command': 'fill','rgb': [255, 0, 255]}")
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    try:
+        print(msg.topic+" "+str(msg.payload))
+        print("?? " + msg.topic + ">= atmosphere/temperature")
+        if str(msg.topic) == "atmosphere/temperature":
+            print(int.from_bytes(msg.payload))
+            if int.from_bytes(msg.payload) >= 25:
+                print("coucou")
+                client.publish("laumio/Laumio0FC168/json", payload="{'command': 'color_wipe', 'duration', 3, 'rgb': [255, 50, 50]}")
+    except Exception as e:
+        print("Oupsi, error detected: " + repr(e))
+
+
+
+
+def on_publish(client, user_data, rc):
+    pass
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
 
 client.connect("mpd.lan", 1883, 60)
-
-client.subscribe("laumio/status/advertise")
-
-client.publish("laumio/all/animate_rainbow")
 
 client.loop_forever()
