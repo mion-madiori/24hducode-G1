f719227114e21d00ef46dda441247a7611eaecac
Cuian <thibault.chauvenet@gmail.com>
2019-01-19 22:54:45 +0100

    Base serveur rest

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ laumio/status/advertise b'Laumio_104F03'
 laumio/status/advertise b'Laumio_107DA8'
 laumio/status/advertise b'Laumio_CD0522'
 laumio/status/advertise b'Laumio_1D9486'
+
+SERVER REST :
+- Executer 'server.py'
+- adresse du serveur : localhost:5000
+- api test factice : localhost:5000/api/people
--- poc/poc2_5_lampColorMultiple.py
+++ poc/poc2_5_lampColorMultiple.py
@@ -8,12 +8,6 @@ def on_connect(client, user_data, flags, rc):
 
     # Connexion
     client.subscribe("laumio/all/discover")
-
-    #client.publish("laumio/Laumio_0FC168/fill", bytes(b'\x255', b'\x0', b'\x255'))
-    #client.publish("laumio/Laumio_0FC168/fill", bytes(b'\x255', b'\x255', b'\x03'))
-    #client.publish("laumio/Laumio_0FC168/fill", bytes(b'\x0255', b'\x0255', b'\x03'))
-    #client.publish("laumio/Laumio_0FC168/fill", bytes(b'255', b'255', b'0'))
-    client.publish("laumio/Laumio_0FC168/fill", bytes([255,255,255]))
     
     
 
@@ -31,8 +25,7 @@ client.connect("mpd.lan", 1883, 60)
 # Execution
 #client.publish("/laumio/NNAAMMEE/color_wipe", "00 80 00 1000")
 #client.publish("/laumio/Laumio_10805F/fill", "00 80 00")
-#client.publish("/laumio/Laumio_0FC168/animate_rainbow")
+client.publish("/laumio/all/fill", "255 255 255")
 #client.publish("/laumio/Laumio_10805F/fill", "0000FF")
-
+1
 client.loop_forever()
-
