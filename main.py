c9add1b2410f507ab722b50d4d1d97f5a595027b
mdupdup <malo.dupont@ynov.com>
2019-01-19 14:36:41 +0100

    working animate rainbow

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, user_data, flags, rc):
+    print("Successfully connected with result code" + str(rc))
+
+    client.publish("laumio/status/advertise")
+
+    client.subscribe("laumio/all/discover")
+    client.publish("/laumio/all/animate_rainbow")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mpd.lan", 1883, 60)
+
+client.subscribe("laumio/status/advertise")
+
+client.publish("laumio/all/animate_rainbow")
+
+client.loop_forever()
