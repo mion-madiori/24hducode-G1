ca4ff3504f6afc453a46a7f91322e3c6f368b9c3
mdupdup <malo.dupont@ynov.com>
2019-01-19 22:51:41 +0100

    working on_message, trying distance amplitude

--- poc5_getCapteursInfo.py
+++ poc5_getCapteursInfo.py
@@ -1,6 +1,14 @@
 import paho.mqtt.client as mqtt
-import time
-import json
+
+
+def pseudo_byte_to_int(byte_value):
+    str_value = ""
+
+    for char in str(byte_value):
+        if char not in ['b','\'']:
+            str_value += char
+
+    return float(str_value)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -11,6 +19,7 @@ def on_connect(client, userdata, flags, rc):
     print("Liste des laumios :")
     client.publish("laumio/all/discover")
     client.subscribe("atmosphere/temperature")
+    client.subscribe("distance/value")
 
     client.publish("laumio/Laumio_0FC168/json", payload="{'command': 'fill','rgb': [255, 0, 255]}")
 
@@ -18,16 +27,25 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     try:
         print(msg.topic+" "+str(msg.payload))
-        print("?? " + msg.topic + ">= atmosphere/temperature")
         if str(msg.topic) == "atmosphere/temperature":
-            print(int.from_bytes(msg.payload))
-            if int.from_bytes(msg.payload) >= 25:
+            print(type(msg.payload))
+            print(pseudo_byte_to_int(msg.payload))
+            if pseudo_byte_to_int(msg.payload) >= 25:
                 print("coucou")
-                client.publish("laumio/Laumio0FC168/json", payload="{'command': 'color_wipe', 'duration', 3, 'rgb': [255, 50, 50]}")
+                client.publish("laumio/Laumio_0FC168/json", payload="{'command': 'fill', 'rgb': [255, 50, 50]}")
+            elif pseudo_byte_to_int(msg.payload) < 25 and pseudo_byte_to_int(msg.payload) >= 10:
+                client.publish("laumio/Laumio_0FC168/json", payload="{'command': 'fill', 'rgb': [255, 120, 120]}")
+            elif pseudo_byte_to_int(msg.payload) < 10:
+                client.publish("laumio/Laumio_0FC168/json", payload="{'command': 'fill', 'rgb': [50, 50, 255]}")
+        elif str(msg.topic) == "distance/value":
+            val = str(pseudo_byte_to_int(msg.payload) * 63)
+            high_val = str(pseudo_byte_to_int(msg.payload * 127))
+            payload = "{'command': 'fill', 'rgb': [" + val + ", " + val + ", " + high_val + "]}"
+            client.publish("laumio/Laumio_OFC168/json", payload=payload)
+
+
     except Exception as e:
         print("Oupsi, error detected: " + repr(e))
-
-
 
 
 def on_publish(client, user_data, rc):
