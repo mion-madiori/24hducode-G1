251045ce9ceb4be63236ddb1ef72fae285f993ca
Vincent CR <crombez.vincent44@gmail.com>
2019-01-19 21:41:18 +0100

    versionJS

--- versionJS/index.js
+++ versionJS/index.js
@@ -11,12 +11,16 @@ client.on('connect', () => {
 
     client.subscribe(`laumio/status/advertise`)
 
-    setInterval(() => {
-        client.publish(`laumio/all/animate_rainbow`)
-    }, 2000);
-    client.publish(`laumio/all/animate_rainbow`);
+    // setInterval(() => {
+    //     client.publish(`laumio/all/animate_rainbow`)
+    // }, 2000);
 
-    client.publish(`laumio/all/discover`);
+    // client.publish(`laumio/all/discover`);
+    
+    client.publish("laumio/all/fill", JSON.stringify({
+        command: 'fill',
+        rgb: [200, 200, 50]
+      }));
 
 })
 
