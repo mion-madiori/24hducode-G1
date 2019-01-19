17e09d1c7894682ca66a284f87a32d293a125546
mdupdup <malo.dupont@ynov.com>
2019-01-19 15:49:36 +0100

    working sensors

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import paho.mqtt.client as mqtt
 def on_connect(client, user_data, flags, rc):
     print("Successfully connected with result code" + str(rc))
 
-    client.publish("laumio/status/advertise")
+    client.subscribe("laumio/status/advertise")
 
-    client.subscribe("laumio/all/discover")
-    client.publish("/laumio/all/animate_rainbow")
+    print("Liste des laumios :")
+    client.publish("laumio/all/discover")
+    client.subscribe("/distance/value")
+
+    client.publish("laumio/Laumio_439BA9/fill", [0,0,255])
 
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+
 
 
 client = mqtt.Client()
@@ -19,9 +23,5 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("mpd.lan", 1883, 60)
-
-client.subscribe("laumio/status/advertise")
-
-client.publish("laumio/all/animate_rainbow")
 
 client.loop_forever()
