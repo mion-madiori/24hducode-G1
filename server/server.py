e8a49d1d19b6170ddeec17a20103c49f3f035efe
mdupdup <malo.dupont@ynov.com>
2019-01-20 05:35:08 +0100

    working server

--- server/server.py
+++ server/server.py
@@ -1,11 +1,30 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 import connexion
+from api.laumio import *
+import json
+
 
 # Création de l'instance de l'application
 app = connexion.App(__name__, specification_dir='./')
 
 # Lecture du fichier swagger.yml pour configurer les endpoints
-app.add_api('swagger.yml')
+# app.add_api('swagger.yml')
+
+
+@app.route('/')
+def hello():
+    return jsonify({'success': True})
+
+
+@app.route('/rainbow')
+def rainboww():
+    rainbow(request)
+
+
+@app.route('/rainbows')
+def rainbows():
+    rainbowAll()
+
 
 # Création d'une route URl dans l'application pour "/"
 @app.route('/')
